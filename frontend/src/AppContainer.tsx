bf6462ca71012553c1f416c50a374b5b56f29aaa
Jore <jori-pekka.rautava@vincit.fi>
2020-04-06 21:36:44 +0300

    Transfer project into TypeScript, add first pages and router

---
+++ frontend/src/AppContainer.tsx
@@ -0,0 +1,47 @@
+/*
+* Navigator for the application
+*/
+
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+
+import Home from './screens/Home';
+import Me from './screens/Me';
+import CV from './screens/CV';
+
+const AppContainer = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/cv">CV</Link>
+            </li>
+            <li>
+              <Link to="/me">Me</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/me">
+            <Me />
+          </Route>
+          <Route path="/cv">
+            <CV />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+      </div>
+    </Router>
+  );
+}
+
+export default AppContainer;
